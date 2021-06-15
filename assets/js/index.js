24c8c290cd08ebe1d4ab273a2b2449de619337a4
HuongNguyen234 <huonghaehae@gmail.com>
2021-06-16 00:33:13 +0700

    validator

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,6 @@ function onModal(){
 
 function offModal(){
     var modal = document.querySelector(".modal");
-   
     modal.classList.add("hiddens");
 }
 
@@ -65,6 +64,62 @@ const expand = () => {
   searchBtn.classList.toggle("close");
   input.classList.toggle("square");
 };
+
+function handleSubmitRegister(name){
+
+  var warningform = document.querySelector('.' + name + ' .warning-form');
+  var inputElement =  document.querySelectorAll('.' + name +' .form__group--input');
+  inputElement = Array.prototype.slice.call(inputElement);
+  var check = true;  
+  inputElement.forEach((elm, index)=>{
+    var inputValue = elm.value;
+    if(inputValue.trim() === ''){
+      check=false;
+      warningform.innerHTML = 'Vui lòng nhập đày đủ thông tin!'
+    }
+  })
+  
+  var pass = document.querySelector("input[name ='pass']");
+  var repass = document.querySelector("input[name ='repass']");
+  if(pass.value != repass.value && check == true){
+    check=false;
+    warningform.innerHTML = 'Mật khẩu không khớp!'
+  }
+  if(check == true){
+    warningform.innerHTML = '';
+    inputElement.forEach((elm, index)=>{
+      elm.value = ''
+    })
+    alert("Đăng ký thành công!");
+  }
+}
+
+function handleSubmitLogin(name){
+
+  var warningform = document.querySelector('.' + name +' .warning-form');
+  var inputElement =  document.querySelectorAll('.' + name +' .form__group--input');
+  inputElement = Array.prototype.slice.call(inputElement);
+  var check = true;  
+  console.log(inputElement)
+  inputElement.forEach((elm, index)=>{
+    var inputValue = elm.value;
+    if(inputValue.trim() === ''){
+      check=false;
+      warningform.innerHTML = 'Vui lòng nhập đày đủ thông tin!'
+    }
+  })
+  
+  if(check == true){
+    warningform.innerHTML = '';
+    var modal = document.querySelector(".modal-Login")
+    modal.classList.add('hiddens')
+  }
+}
+
+function handleCancel(){
+  var modal = document.querySelector(".modal")
+  modal.classList.add('hiddens')
+}
 
 searchBtn.addEventListener("click", expand);
 
